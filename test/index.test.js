6b29ab5c51061eb31b71de836fe94c21c3d233ec
Alec Ferguson <alec.ferguson@creditkarma.com>
2017-01-26 12:09:59 -0800

    feat(influx-udp) Added UDP support to Good Influx.

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@ const GoodInflux = require('../lib/index')
 
 const Stream = require('stream')
 const Http = require('http')
+const Dgram = require('dgram')
 
 const Code = require('code')
 const Lab = require('lab')
@@ -40,6 +41,8 @@ const testEvent = {
         responseTimes: {}
     }
 }
+/* eslint max-len: ["error", 440, 4] */
+const expectedMessage = 'ops,host=mytesthost,pid=9876 os.cpu1m=1.8408203125,os.cpu5m=1.44287109375,os.cpu15m=1.15234375,os.freemem=162570240i,os.totalmem=6089818112i,os.uptime=11546i,proc.delay=0.07090700045228004,proc.heapTotal=41546080i,proc.heapUsed=27708712i,proc.rss=55812096i,proc.uptime=18.192,testing="superClutch" 123456789000000'
 
 const mocks = {
     readStream() {
@@ -50,12 +53,13 @@ const mocks = {
         return result
     },
 
-    getUri(server) {
+    getUri(server, protocol) {
         const address = server.address()
-        return `http://${address.address}:${address.port}`
+        return `${protocol}://${address.address}:${address.port}`
     },
 
-    getServer(done) {
+    getHttpServer(done) {
+        let hitCount = 0
         const server = Http.createServer((req, res) => {
             let data = ''
 
@@ -63,40 +67,91 @@ const mocks = {
                 data += chunk
             })
             req.on('end', () => {
+                hitCount += 1
                 const dataRows = data.split('\n')
-                expect(dataRows.length).to.be.greaterThan(1)
 
+                // Because threshold is 5, expect 5 events to be sent at a time
+                expect(dataRows.length).to.equal(5)
                 dataRows.forEach((datum) => {
-                    /* eslint max-len: ["error", 880, 4] */
-                    expect(datum).to.equal('ops,host=mytesthost,pid=9876 os.cpu1m=1.8408203125,os.cpu5m=1.44287109375,os.cpu15m=1.15234375,os.freemem=162570240i,os.totalmem=6089818112i,os.uptime=11546i,proc.delay=0.07090700045228004,proc.heapTotal=41546080i,proc.heapUsed=27708712i,proc.rss=55812096i,proc.uptime=18.192,testing="superClutch" 123456789000000')
+                    expect(datum).to.equal(expectedMessage)
                 })
 
                 res.end()
-                server.close(done)
+                if (hitCount >= 2) {
+                    server.close(done)
+                }
             })
         })
 
+        return server
+    },
+
+    getUdpServer(done) {
+        let hitCount = 0
+        const server = Dgram.createSocket('udp4')
+        server.on('message', (msg) => {
+            hitCount += 1
+            const splitMessage = msg.toString().split('\n')
+
+            // Because threshold is 5, expect 5 events to be sent at a time
+            expect(splitMessage.length).to.equal(5)
+            splitMessage.forEach((msgRow) => {
+                expect(msgRow).to.equal(expectedMessage)
+            })
+            if (hitCount >= 2) {
+                server.close(done)
+            }
+        })
+        server.bind(9876, '127.0.0.1')
         return server
     }
 }
 
 describe('GoodInflux', () => {
-    it('Sends events in a stream', (done) => {
-        const server = mocks.getServer(done)
-
+    it('Http URL => Sends events in a stream to HTTP server', (done) => {
+        const server = mocks.getHttpServer(done)
         const stream = mocks.readStream()
 
         server.listen(0, '127.0.0.1', () => {
-            const reporter = new GoodInflux(mocks.getUri(server), {
+            const reporter = new GoodInflux(mocks.getUri(server, 'http'), {
                 threshold: 5,
                 metadata: { testing: 'superClutch' }
             })
 
             stream.pipe(reporter)
 
-            for (let i = 0; i < 5; i += 1) {
+            // Important to send 10 events. Threshold is 5, so two batches of events are sent.
+            // Sending two batches proves that the callback is being passed properly to Wreck.request.
+            for (let i = 0; i < 10; i += 1) {
                 stream.push(testEvent)
             }
         })
+    })
+
+    it('Udp URL => Sends events in a stream to UDP server', (done) => {
+        const server = mocks.getUdpServer(done)
+        const stream = mocks.readStream()
+
+        server.on('listening', () => {
+            const reporter = new GoodInflux(mocks.getUri(server, 'udp'), {
+                threshold: 5,
+                metadata: { testing: 'superClutch' }
+            })
+
+            stream.pipe(reporter)
+
+            // Important to send 10 events. Threshold is 5, so two batches of events are sent.
+            // Sending two batches proves that the callback is being passed properly to this._udpClient.send.
+            for (let i = 0; i < 10; i += 1) {
+                stream.push(testEvent)
+            }
+        })
+    })
+
+    it('Unsupported protocol => throw error', (done) => {
+        expect(() => {
+            return new GoodInflux('ftp://abcd:1234', {})
+        }).to.throw(Error, 'Unsupported protocol ftp:. Supported protocols are udp, http or https')
+        done()
     })
 })
