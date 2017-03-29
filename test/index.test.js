50681002d6de7bebe4684d9397ac70ff7de1db89
wei.hai <wei.hai@creditkarma.com>
2017-03-28 17:29:46 -0700

    Feat (ops-load) Send metrics of ops.load to influxdb

--- test/index.test.js
+++ test/index.test.js
@@ -36,19 +36,61 @@ const testEvent = {
         delay: 0.07090700045228004
     },
     load: {
-        requests: {},
-        concurrents: { '8080': 0 },
-        responseTimes: {}
+        requests: {
+            8080: {
+                total: 9,
+                disconnects: 0,
+                statusCodes: {
+                    200: 9
+                }
+            }
+        },
+        concurrents: { 8080: 1 },
+        responseTimes: {
+            8080: {
+                avg: 999,
+                max: 2222
+            }
+        },
+        sockets: {
+            http: {
+                total: 0
+            },
+            https: {
+                total: 2
+            }
+        }
     }
 };
-/* eslint max-len: ["error", 440, 4] */
-const expectedMessage = 'ops,host=mytesthost,pid=9876 os.cpu1m=1.8408203125,os.cpu5m=1.44287109375,os.cpu15m=1.15234375,os.freemem=162570240i,os.totalmem=6089818112i,os.uptime=11546i,proc.delay=0.07090700045228004,proc.heapTotal=41546080i,proc.heapUsed=27708712i,proc.rss=55812096i,proc.uptime=18.192,testing="superClutch" 123456789000000';
+
+/**
+ * TODO: Find some way to make sure this has actually been hit
+ *
+ * Checking that the events sent to InfluxDB:
+ *  1) Starts with "ops"
+ *  2) Contains the custom metadata specified
+ *
+ * Not very comprehensive validation of the events in this test since more comprehensive
+ * testing is done in line-protocol.test.js.
+ *
+ * @param [String] responseData
+ */
+const validateResponses = (responseData, expectedEvents) => {
+    const expectedLength = expectedEvents || 25;
+    const dataRows = responseData.split('\n');
+    // Because threshold is 5, expect 5 events to be sent at a time
+    // Since 5 influx events are emitted per ops event, expect length to equal 25
+    expect(dataRows.length).to.equal(expectedLength);
+    dataRows.forEach((datum) => {
+        expect(datum).to.match(/^ops/);
+        expect(datum).to.match(/testing="superClutch"/);
+    });
+};
 
 const mocks = {
     readStream() {
         const result = new Stream.Readable({ objectMode: true });
-        // Need to overwrite this function. For some reason all it does is Error('not implemented')  Very helpful, no?
-        /* eslint-disable no-empty-function */
+        // Need to overwrite this function. For some reason all it does is Error('not implemented').
         result._read = () => {};
         return result;
     },
@@ -68,13 +110,7 @@ const mocks = {
             });
             req.on('end', () => {
                 hitCount += 1;
-                const dataRows = data.split('\n');
-
-                // Because threshold is 5, expect 5 events to be sent at a time
-                expect(dataRows.length).to.equal(5);
-                dataRows.forEach((datum) => {
-                    expect(datum).to.equal(expectedMessage);
-                });
+                validateResponses(data);
 
                 res.end();
                 if (hitCount >= 2) {
@@ -86,18 +122,13 @@ const mocks = {
         return server;
     },
 
-    getUdpServer(done) {
+    getUdpServer(expectedNumberOfEvents, done) {
         let hitCount = 0;
         const server = Dgram.createSocket('udp4');
         server.on('message', (msg) => {
             hitCount += 1;
-            const splitMessage = msg.toString().split('\n');
+            validateResponses(msg.toString(), expectedNumberOfEvents);
 
-            // Because threshold is 5, expect 5 events to be sent at a time
-            expect(splitMessage.length).to.equal(5);
-            splitMessage.forEach((msgRow) => {
-                expect(msgRow).to.equal(expectedMessage);
-            });
             if (hitCount >= 2) {
                 server.close(done);
             }
@@ -108,43 +139,86 @@ const mocks = {
 };
 
 describe('GoodInflux', () => {
-    it('Http URL => Sends events in a stream to HTTP server', (done) => {
-        const server = mocks.getHttpServer(done);
-        const stream = mocks.readStream();
+    describe('Http URL =>', () => {
+        it('Sends events in a stream to HTTP server', (done) => {
+            const server = mocks.getHttpServer(done);
+            const stream = mocks.readStream();
 
-        server.listen(0, '127.0.0.1', () => {
-            const reporter = new GoodInflux(mocks.getUri(server, 'http'), {
-                threshold: 5,
-                metadata: { testing: 'superClutch' }
+            server.listen(0, '127.0.0.1', () => {
+                const reporter = new GoodInflux(mocks.getUri(server, 'http'), {
+                    threshold: 5,
+                    metadata: { testing: 'superClutch' }
+                });
+
+                stream.pipe(reporter);
+
+                // Important to send 10 events. Threshold is 5, so two batches of events are sent.
+                // Sending two batches proves that the callback is being passed properly to Wreck.request.
+                for (let i = 0; i < 10; i += 1) {
+                    stream.push(testEvent);
+                }
             });
-
-            stream.pipe(reporter);
-
-            // Important to send 10 events. Threshold is 5, so two batches of events are sent.
-            // Sending two batches proves that the callback is being passed properly to Wreck.request.
-            for (let i = 0; i < 10; i += 1) {
-                stream.push(testEvent);
-            }
         });
     });
 
-    it('Udp URL => Sends events in a stream to UDP server', (done) => {
-        const server = mocks.getUdpServer(done);
-        const stream = mocks.readStream();
+    describe('Udp URL =>', () => {
+        it('Threshold not set => Sends 25 events in a stream to UDP server', (done) => {
+            const server = mocks.getUdpServer(25, done);
+            const stream = mocks.readStream();
 
-        server.on('listening', () => {
-            const reporter = new GoodInflux(mocks.getUri(server, 'udp'), {
-                threshold: 5,
-                metadata: { testing: 'superClutch' }
+            server.on('listening', () => {
+                const reporter = new GoodInflux(mocks.getUri(server, 'udp'), {
+                    metadata: { testing: 'superClutch' }
+                });
+
+                stream.pipe(reporter);
+
+                // Important to send 10 events. Threshold is 5, so two batches of events are sent.
+                // Sending two batches proves that the callback is being passed properly to this._udpClient.send.
+                for (let i = 0; i < 10; i += 1) {
+                    stream.push(testEvent);
+                }
             });
+        });
 
-            stream.pipe(reporter);
+        it('Threshold of 3 => Sends 15 events in a stream to UDP server', (done) => {
+            const server = mocks.getUdpServer(15, done);
+            const stream = mocks.readStream();
 
-            // Important to send 10 events. Threshold is 5, so two batches of events are sent.
-            // Sending two batches proves that the callback is being passed properly to this._udpClient.send.
-            for (let i = 0; i < 10; i += 1) {
-                stream.push(testEvent);
-            }
+            server.on('listening', () => {
+                const reporter = new GoodInflux(mocks.getUri(server, 'udp'), {
+                    threshold: 3,
+                    metadata: { testing: 'superClutch' }
+                });
+
+                stream.pipe(reporter);
+
+                // Important to send 6 events. Threshold is 3, so two batches of events are sent.
+                // Sending two batches proves that the callback is being passed properly to this._udpClient.send.
+                for (let i = 0; i < 6; i += 1) {
+                    stream.push(testEvent);
+                }
+            });
+        });
+
+        it('Threshold of 13 => Sends 25 events in a stream to UDP server', (done) => {
+            const server = mocks.getUdpServer(25, done);
+            const stream = mocks.readStream();
+
+            server.on('listening', () => {
+                const reporter = new GoodInflux(mocks.getUri(server, 'udp'), {
+                    threshold: 13,
+                    metadata: { testing: 'superClutch' }
+                });
+
+                stream.pipe(reporter);
+
+                // Important to send 10 events. Threshold is 5, so two batches of events are sent.
+                // Sending two batches proves that the callback is being passed properly to this._udpClient.send.
+                for (let i = 0; i < 10; i += 1) {
+                    stream.push(testEvent);
+                }
+            });
         });
     });
 
