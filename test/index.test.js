ebe11ea1b8144c23efdee71ee64ae5d5030839cc
Fabriece Sumuni <sfabriece@users.noreply.github.com>
2017-05-29 16:07:33 +0200

    fix(): update line-protocol to include metadata in the Tag set (#11)
    * fix(): update line-protocol to include metadata in the Tag set
    
    * refactor(): move tag extraction code to internals.tags
    
    refactor tests to make sure 0 is not treated as an empty value

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,8 @@
 
 const GoodInflux = require('../lib/index');
 
+const Hoek = require('hoek');
+
 const Stream = require('stream');
 const Http = require('http');
 const Dgram = require('dgram');
@@ -23,7 +25,8 @@ const testEvent = {
         load: [1.8408203125, 1.44287109375, 1.15234375],
         mem: {
             total: 6089818112,
-            free: 162570240 },
+            free: 162570240
+        },
         uptime: 11546
     },
     proc: {
@@ -42,14 +45,15 @@ const testEvent = {
     }
 };
 /* eslint max-len: ["error", 440, 4] */
-const expectedMessage = 'ops,host=mytesthost,pid=9876 os.cpu1m=1.8408203125,os.cpu5m=1.44287109375,os.cpu15m=1.15234375,os.freemem=162570240i,os.totalmem=6089818112i,os.uptime=11546i,proc.delay=0.07090700045228004,proc.heapTotal=41546080i,proc.heapUsed=27708712i,proc.rss=55812096i,proc.uptime=18.192,testing="superClutch" 123456789000000';
+const expectedMessage = 'ops,host=mytesthost,pid=9876,testing="superClutch",version="0",name="" os.cpu1m=1.8408203125,os.cpu5m=1.44287109375,os.cpu15m=1.15234375,os.freemem=162570240i,os.totalmem=6089818112i,os.uptime=11546i,proc.delay=0.07090700045228004,proc.heapTotal=41546080i,proc.heapUsed=27708712i,proc.rss=55812096i,proc.uptime=18.192 123456789000000';
+const msgWithoutMetadata = 'ops,host=mytesthost,pid=9876 os.cpu1m=1.8408203125,os.cpu5m=1.44287109375,os.cpu15m=1.15234375,os.freemem=162570240i,os.totalmem=6089818112i,os.uptime=11546i,proc.delay=0.07090700045228004,proc.heapTotal=41546080i,proc.heapUsed=27708712i,proc.rss=55812096i,proc.uptime=18.192 123456789000000';
 
 const mocks = {
     readStream() {
         const result = new Stream.Readable({ objectMode: true });
         // Need to overwrite this function. For some reason all it does is Error('not implemented')  Very helpful, no?
         /* eslint-disable no-empty-function */
-        result._read = () => {};
+        result._read = () => { };
         return result;
     },
 
@@ -58,7 +62,7 @@ const mocks = {
         return `${protocol}://${address.address}:${address.port}`;
     },
 
-    getHttpServer(done) {
+    getHttpServer(expectedMsg, done) {
         let hitCount = 0;
         const server = Http.createServer((req, res) => {
             let data = '';
@@ -73,7 +77,7 @@ const mocks = {
                 // Because threshold is 5, expect 5 events to be sent at a time
                 expect(dataRows.length).to.equal(5);
                 dataRows.forEach((datum) => {
-                    expect(datum).to.equal(expectedMessage);
+                    expect(datum).to.equal(expectedMsg);
                 });
 
                 res.end();
@@ -107,16 +111,22 @@ const mocks = {
     }
 };
 
+const Options = {
+    threshold: 5,
+    metadata: {
+        testing: 'superClutch',
+        version: 0,
+        name: ''
+    }
+};
+
 describe('GoodInflux', () => {
     it('Http URL => Sends events in a stream to HTTP server', (done) => {
-        const server = mocks.getHttpServer(done);
+        const server = mocks.getHttpServer(expectedMessage, done);
         const stream = mocks.readStream();
 
         server.listen(0, '127.0.0.1', () => {
-            const reporter = new GoodInflux(mocks.getUri(server, 'http'), {
-                threshold: 5,
-                metadata: { testing: 'superClutch' }
-            });
+            const reporter = new GoodInflux(mocks.getUri(server, 'http'), Options);
 
             stream.pipe(reporter);
 
@@ -133,10 +143,7 @@ describe('GoodInflux', () => {
         const stream = mocks.readStream();
 
         server.on('listening', () => {
-            const reporter = new GoodInflux(mocks.getUri(server, 'udp'), {
-                threshold: 5,
-                metadata: { testing: 'superClutch' }
-            });
+            const reporter = new GoodInflux(mocks.getUri(server, 'udp'), Options);
 
             stream.pipe(reporter);
 
@@ -153,5 +160,65 @@ describe('GoodInflux', () => {
             return new GoodInflux('ftp://abcd:1234', {});
         }).to.throw(Error, 'Unsupported protocol ftp:. Supported protocols are udp, http or https');
         done();
+    });
+
+    it('Omit metadata when not defined', (done) => {
+        const server = mocks.getHttpServer(msgWithoutMetadata, done);
+        const stream = mocks.readStream();
+
+        server.listen(0, '127.0.0.1', () => {
+            const reporter = new GoodInflux(mocks.getUri(server, 'http'), {
+                threshold: 5
+            });
+
+            stream.pipe(reporter);
+
+            // Important to send 10 events. Threshold is 5, so two batches of events are sent.
+            // Sending two batches proves that the callback is being passed properly to Wreck.request.
+            for (let i = 0; i < 10; i += 1) {
+                stream.push(testEvent);
+            }
+        });
+    });
+
+    it('Omit metadata when empty', (done) => {
+        const server = mocks.getHttpServer(msgWithoutMetadata, done);
+        const stream = mocks.readStream();
+
+        server.listen(0, '127.0.0.1', () => {
+            const reporter = new GoodInflux(mocks.getUri(server, 'http'), {
+                threshold: 5,
+                metadata: {}
+            });
+
+            stream.pipe(reporter);
+
+            // Important to send 10 events. Threshold is 5, so two batches of events are sent.
+            // Sending two batches proves that the callback is being passed properly to Wreck.request.
+            for (let i = 0; i < 10; i += 1) {
+                stream.push(testEvent);
+            }
+        });
+    });
+
+    it('Omit undefined metadata', (done) => {
+        const server = mocks.getHttpServer(expectedMessage, done);
+        const stream = mocks.readStream();
+
+        const opts = Hoek.clone(Options);
+        opts.metadata.notDefined = undefined;
+        opts.metadata.isNull = null;
+
+        server.listen(0, '127.0.0.1', () => {
+            const reporter = new GoodInflux(mocks.getUri(server, 'http'), opts);
+
+            stream.pipe(reporter);
+
+            // Important to send 10 events. Threshold is 5, so two batches of events are sent.
+            // Sending two batches proves that the callback is being passed properly to Wreck.request.
+            for (let i = 0; i < 10; i += 1) {
+                stream.push(testEvent);
+            }
+        });
     });
 });
